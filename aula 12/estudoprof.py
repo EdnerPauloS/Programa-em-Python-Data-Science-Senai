1e3146dd64ce569193ccdb7d75b1406675abebe7
EdnerPauloS <edner.paulo.dias@gmail.com>
2025-04-24 21:57:41 -0300

    Add files via upload

---
+++ aula 12/estudoprof.py
@@ -0,0 +1,44 @@
+import seaborn as sns
+import matplotlib.pyplot as plt
+import pandas as pd 
+import tkinter as tk
+from tkinter import filedialog
+
+
+def carregar():
+    file = filedialog.askopenfilename()
+    df = pd.read_excel(file)
+    plt.figure(figsize=(10,6))
+    sns.barplot(x='mês',y='vendas', data=df)
+    plt.show()
+
+
+def carregar2():
+    file = filedialog.askopenfilename()
+    df = pd.read_excel(file)
+    plt.figure(figsize=(10,6))
+    sns.lineplot(x='mês',y='vendas', data=df)
+    plt.show()
+
+
+
+def carregar3():
+    file = filedialog.askopenfilename()
+    df = pd.read_excel(file)
+    plt.figure(figsize=(10,6))
+    sns.scatterplot(x='vendedor', y ='vendas', data=df)
+    plt.show()    
+
+
+
+
+janela = tk.Tk()
+
+
+btn = tk.Button(janela, text='clique', command=carregar3)
+btn.pack()
+
+
+janela.mainloop()    
+
+
